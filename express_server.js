cc1d6f770385b465582d88939c7fe66808114311
Camilo Gomez <camilogomezmusic@gmail.com>
2020-08-03 19:10:57 +0000

    Created function to generate a random string and added an event handler for a POST request made by the urls_new page

--- express_server.js
+++ express_server.js
@@ -8,6 +8,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+const generateRandomString = function() {
+  return Math.random().toString(36).substring(2,8);
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -33,10 +37,19 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
-})
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
   
-})
+});
 
+
+
+
+console.log(generateRandomString());
