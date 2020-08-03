c4a54c9d6543c4d36fd0c200e5bd685e66ef0f0c
Camilo Gomez <camilogomezmusic@gmail.com>
2020-08-03 18:58:40 +0000

    added handler for get request for the new url form page

--- express_server.js
+++ express_server.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,9 +26,9 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html?\n');
-});
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new')
+;});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
