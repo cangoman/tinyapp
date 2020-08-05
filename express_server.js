1462d6afff22a05f04a331f89b1a4cc26ba7058d
Camilo Gomez <camilogomezmusic@gmail.com>
2020-08-05 20:16:09 +0000

    Added an error page that displays and error sent by the server

--- express_server.js
+++ express_server.js
@@ -55,20 +55,20 @@ const formatHTTP = function(address) {
 };
 
 const urlsForUser = function(id) {
-    const urlsForUser = {};
-    for (let url in urlDatabase) {
-      if (id === urlDatabase[url].userID) {
-        urlsForUser[url] = urlDatabase[url];
-      }
+  const urlsForUser = {};
+  for (let url in urlDatabase) {
+    if (id === urlDatabase[url].userID) {
+      urlsForUser[url] = urlDatabase[url];
     }
-    return urlsForUser;
+  }
+  return urlsForUser;
 };
 
 
 /*--------------REQUEST HANDLERS-----------------*/
 
 // GET REQUESTS
-app.get("/", (req, res) => { 
+app.get("/", (req, res) => {
   if (!req.session.user_id) {
     res.redirect('/login');
   } else {
@@ -82,45 +82,58 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get('/urls.json', (req, res) => { // unnecessary?
-  res.json(urlDatabase);
-});
+// app.get('/urls.json', (req, res) => { // unnecessary?
+//   res.json(urlDatabase);
+// });
 
 app.get("/urls/new", (req, res) => {
   if (!req.session.user_id) {
     res.redirect('/login');
   } else {
-    let templateVars = {user: users[req.session.user_id]}
+    let templateVars = {user: users[req.session.user_id]};
     res.render('urls_new', templateVars);
   }
-;});
-
-app.get("/urls/:id", (req, res) => {
-  if (!urlDatabase[req.params.id]) {
-    res.status(400).send('Invalid URL');
-  }
-  let templateVars = {shortURL: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: users[req.session.user_id] };
-  res.render('urls_show', templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => { 
-  const longURL = urlDatabase[req.params["shortURL"]].longURL;
-  res.redirect(longURL);
+app.get("/urls/:id", (req, res) => {
+  if (!req.session.user_id) {
+    res.status(400).redirect('/login');
+  } else {
+    const url = urlDatabase[req.params.id];
+    if (!url || url.id !== req.session.user_id) {
+      const errorMessage = "The URL requested does not exist, or you don't have the appropriate permissions"
+      let templateVars = {shortURL: req.params.id, error: errorMessage, user: users[req.session.user_id]};
+      res.render('error', templateVars);
+    } else {
+      let templateVars = {shortURL: req.params.id, longURL: url.longURL, user: users[req.session.user_id] };
+      res.render('urls_show', templateVars);
+    }
+  }
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = (urlDatabase[req.params["id"]]) ? urlDatabase[req.params["id"]].longURL : null;
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    const errorMessage = "The requested URL does not exist";
+    let templateVars = {shortURL: req.params.id, error: errorMessage, user: users[req.session.user_id]};
+    res.render('error', templateVars);
+  }
 });
 
 app.get("/register", (req,res) => {
   if (!req.session.user_id) {
-  let templateVars = {user: users[req.session.user_id] };
-  res.render("register", templateVars);
+    let templateVars = {user: users[req.session.user_id] };
+    res.render("register", templateVars);
   } else {
     res.redirect('/urls');
   }
-
 });
 
 app.get('/login', (req, res) => {
   if (!req.session.user_id) {
-    let templateVars = {user: users[req.session.user_id]}
+    let templateVars = {user: users[req.session.user_id]};
     res.render('login', templateVars);
   } else {
     res.redirect('/urls');
@@ -136,7 +149,7 @@ app.post("/urls", (req, res) => {
   if (user) {
     const shortURL = generateRandomString();
     urlDatabase[shortURL] = { longURL: formatHTTP(req.body.longURL), userID: user };
-    res.redirect(302, `/urls/${shortURL}`)
+    res.redirect(302, `/urls/${shortURL}`);
   } else {
     res.redirect('/urls');
   }
@@ -147,7 +160,7 @@ app.post('/urls/:id', (req, res) => {
   const id = req.params.id;
   if (req.session["user_id"] === urlDatabase[id].userID) {
     const newURL = req.body.longURL;
-    urlDatabase[id].longURL = formatHTTP(newURL);  
+    urlDatabase[id].longURL = formatHTTP(newURL);
   }
   res.redirect('/urls');
 });
@@ -155,17 +168,17 @@ app.post('/urls/:id', (req, res) => {
 //Deletes a URL from the database
 app.post('/urls/:shortURL/delete', (req, res) => {
   const id = req.params.shortURL;
-  if (req.session.user_id === urlDatabase[id].userID){
+  if (req.session.user_id === urlDatabase[id].userID) {
     delete urlDatabase[req.params.shortURL];
   }
   res.redirect("/urls");
 });
 
 //login form handler, also creates a cookie
-app.post('/login', (req, res) => {  // 
+app.post('/login', (req, res) => {  //
   
   const user = getUserByEmail(req.body.email, users);
-  if (!user || ( !bcrypt.compareSync(req.body.password, user.password)) ) {
+  if (!user || (!bcrypt.compareSync(req.body.password, user.password))) {
     res.sendStatus(403);
   } else {
     req.session.user_id = user.id;
